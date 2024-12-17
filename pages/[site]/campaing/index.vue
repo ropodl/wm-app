c5b11edba24d377933fbb3ea994dbf65a74795a8
Aditys shrestha <aditya1stha@gmail.com>
2024-12-17 22:08:30 +0545

    saroj dai

---
+++ pages/[site]/campaing/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+definePageMeta({
+  layout: "user",
+  middleware: ["user-auth"],
+});
+</script>
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Create a campaing</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
